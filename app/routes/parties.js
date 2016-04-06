b55a05d6f3fd149925ae2ed99c7f11beb1420c64
Jackson Cafazzo <doctorjuno@gmail.com>
2016-04-05 18:31:38 -0700

    change parties link to get all parties by legislator id

--- app/routes/parties.js
+++ app/routes/parties.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-
-   var url = 'http://politicalpartytime.org/api/v1/event/?format=json&beneficiaries__state='+ params.state + '&apikey=a9c257b102d6474f89312633237f65aa';
+    console.log(params);
+   var url = 'http://politicalpartytime.org/api/v1/event/?format=json&beneficiaries__crp_id='+ params.state + '&apikey=a9c257b102d6474f89312633237f65aa';
 
    return Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON;
